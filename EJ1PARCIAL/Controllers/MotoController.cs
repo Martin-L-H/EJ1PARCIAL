874cc06aae7c7e342fb0ee95821146cf1037ba8f
idontknowhowbuttheyfoundme <108311027+idontknowhowbuttheyfoundme@users.noreply.github.com>
2023-05-23 19:11:12 -0300

    Add project files.

---
+++ EJ1PARCIAL/Controllers/MotoController.cs
@@ -0,0 +1,43 @@
+﻿using EJ1PARCIAL.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace EJ1PARCIAL.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MotoController : ControllerBase
+    {
+        private List<Moto> Motos = new List<Moto>();
+        public MotoController()
+        {
+            Motos.Add(new Moto() { Id = 0, Marca = "John", Modelo = "Doe" });
+            Motos.Add(new Moto() { Id = 1, Marca = "Yamaha", Modelo = "ZR-1" });
+            Motos.Add(new Moto() { Id = 2, Marca = "Kawasaki", Modelo = "Ninja" });
+            Motos.Add(new Moto() { Id = 3, Marca = "Suzuki", Modelo = "Hayabusa" });
+            Motos.Add(new Moto() { Id = 4, Marca = "Honda", Modelo = "GoldWing" });
+        }
+
+        [HttpGet]
+        //[Produces("application/xml")]
+        public List<Moto> GetAll()        {
+            return Motos;
+        }
+        [HttpGet("{id}")]
+        //[Produces("application/xml")]
+        public ActionResult<Moto> GetOne(int id)
+        {
+            var unica = Motos.FirstOrDefault(x => x.Id == id);
+            if (unica == null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                return unica;
+            }
+        }
+    }
+}
---
+++ EJ1PARCIAL/Models/Moto.cs
@@ -0,0 +1,9 @@
+﻿namespace EJ1PARCIAL.Models
+{
+    public class Moto
+    {
+        public int Id { get; set; }
+        public string Marca { get; set; }
+        public string Modelo { get; set; }
+    }
+}
